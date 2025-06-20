3df7fc5fc8f91b83ff249161218b9636ab85efaa
= <viktor.brandmaier@icloud.com>
2025-06-20 15:51:58 +0200

    added Test file

---
+++ Test.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def output_misses(data_list, network):
+    
+    for i in range(len(data_list)-1):
+        values = data_list[i].split(',')
+        correct_label = int(values[0])
+        scaled_inputs = (np.asfarray(values[1:])/ 255.0 * 0.99) + 0.1
+        outputs = network.query(scaled_inputs)
+
+        label = np.argmax(outputs)
+        if label != correct_label:
+            print("Network guess:", label, "correct Label:", correct_label, "index:", i)
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import Train
+import Test
 import pickle
+import numpy as np
 
 #defines path for the trained-network file
 nn_trained_file = "nn.pk1"
@@ -11,7 +13,7 @@ try:
         file.close()
     print("loaded trained network")
 
-#if there is no saved, trained network, a network instance is created, trained and tests 
+#if there is no saved, trained network, a network instance is created, trained and tested
 except FileNotFoundError:
 
     Train.miniBatchGradientDescent(*Train.serializeData())
@@ -24,3 +26,8 @@ except FileNotFoundError:
         print ("saved trained network")
     else:
         print ("Network did not meet training standarts")
+
+training_data = Train.data_list_test
+
+#outputs the falsely classified image for better understanding of the networks perfomance
+Test.output_misses(training_data, trained_network_loaded)
